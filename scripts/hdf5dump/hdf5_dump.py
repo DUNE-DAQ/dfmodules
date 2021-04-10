3020cf0f921863dc4eea92d3cfc6a3f926441348
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-04-10 12:01:21 -0500

    clean up hdf5_dump.py to be PEP8 compliant; changed argument options

--- scripts/hdf5dump/hdf5_dump.py
+++ scripts/hdf5dump/hdf5_dump.py
@@ -1,199 +1,194 @@
 #!/usr/bin/python3
 
-# Version 0.1
-# Last modified: January 23, 2021
+# Version 1.0
+# Last modified: April 10, 2021
 
-# USAGE: python3 hdf5_dump.py -f sample.hdf5 -TRH
+# USAGE: python3 hdf5_dump.py -f sample.hdf5
 
 import argparse
-import struct
 
 import h5py
 import datetime
 
+import struct
+
 CLOCK_SPEED_HZ = 50000000.0
 
-## Print FragmentHeader
-def print_frag_header(data_array):
-  (check_word, version, fragment_size,
-   trigger_number, trigger_timestamp,
-   window_begin, window_end,
-   runumber, geo_id_apa, geo_id_link,
-   error_bits, fragment_type
-   ) = struct.unpack('<2I5Q5I', data_array[:68])
-   # https://docs.python.org/3/library/struct.html
-   # Do a h5dump on the hdf5 file to see the datatype
-   # "H5T_STD_I8LE" means, 8 bit little endian standard type
-   # https://support.hdfgroup.org/HDF5/doc/RM/PredefDTypes.html
+g_trigger_record_nfragments = []
+g_trigger_record_nexp_fragments = []
+g_ith_record = -1
 
-  print("Magic word:\t\t", hex(check_word))
-  print("Version:\t\t", version)
-  print("Frag size:\t\t", fragment_size)
-  print("Trig number:\t\t", trigger_number)
-  print("Trig timestamp:\t\t {} ({})".format(trigger_timestamp,
-        datetime.datetime.fromtimestamp(float(trigger_timestamp)/CLOCK_SPEED_HZ)))
-  print("Window begin (ticks@50MHz):\t", window_begin)
-  print("Window end (ticks@50MHz):\t", window_end)
-  print("Run number:\t\t", runumber)
-  print("GeoID (APA):\t\t", geo_id_apa)
-  print("GeoID (link):\t\t", geo_id_link)
-  print("Error bits:\t\t", error_bits)
-  print("Fragment type:\t\t", fragment_type)
+g_n_printed = 0
+g_n_request = 0
+g_header_type = "both"
+g_list_components = False
+
+def tick_to_timestamp(ticks):
+    ns = float(ticks)/CLOCK_SPEED_HZ
+    return datetime.datetime.fromtimestamp(ns)
 
 
-## Print TriggerRecordHeader
-def print_trh(data_array):
-  (check_word, version, trigger_number,
-   trigger_timestamp, numb_req_comp, run_number,
-   error_bits, trigger_type) = struct.unpack('<2I3Q2IH', data_array[:42])
+def unpack_header(data_array, unpack_string, keys):
+    values = struct.unpack(unpack_string, data_array)
+    header = dict(zip(keys, values))
+    return header
 
-  print("Magic word:\t\t", hex(check_word))
-  print("Version:\t\t", version)
-  print("Trig number:\t\t", trigger_number)
-  print("Trig timestamp:\t\t", trigger_timestamp,
-        "("+str(datetime.datetime.fromtimestamp(float(trigger_timestamp)/CLOCK_SPEED_HZ))+")")
-  print("Num req comp:\t\t", numb_req_comp)
-  print("Run number:\t\t", run_number)
-  print("Error bits:\t\t", error_bits)
-  print("Trigger type:\t\t", trigger_type)
 
-  if listComponents:
-      j = 0
-      for (i_geo_id_apa, i_geo_id_link, i_begin, i_end) in struct.iter_unpack("<2I2Q", data_array[48:]):
-          print("Expected Component: \t\t", j)
-          print("       GeoID (APA): \t\t", i_geo_id_apa)
-          print("       GeoID (link): \t\t", i_geo_id_link)
-          #print("       Begin time (ticks@50MHz): \t", i_begin)
-          #print("       End time (ticks@50MHz): \t\t", i_end)
-          j += 1
-
-  return
-
-# Actions
-
-trigger_record_fragments = []
-trigger_record_header_expected_fragments = []
-
-def examine_fragments(name, dset):
-    global nheader
-    if isinstance(dset, h5py.Group) and "/" not in name:
-        # This is a new trigger record.
-        nheader += 1
-        trigger_record_fragments.append(0)
-    if isinstance(dset, h5py.Dataset) and "FELIX" in name:
-        # This is a new fragment
-        trigger_record_fragments[nheader] += 1
-    if isinstance(dset, h5py.Dataset) and "TriggerRecordHeader" in name:
-        # This is a new TriggerRecordHeader
-        data_array = bytearray(dset[:])
-        (i,) = struct.unpack('<Q', data_array[24:32])
-        trigger_record_header_expected_fragments.append(i)
+def print_header(hdict):
+    for ik, iv in hdict.items():
+        if "timestamp" in ik:
+            print("{:<30}: {} ({})".format(
+                ik, iv, tick_to_timestamp(iv)))
+        elif 'Magic word' in ik:
+            print("{:<30}: {}".format(ik, hex(iv)))
+        else:
+            print("{:<30}: {}".format(ik, iv))
     return
 
-def print_header_func(name, dset):
-    global nheader
-    if nheader < NHEADER and isinstance(dset, h5py.Dataset):
-        if printTRH and "TriggerRecordHeader" in name:
-            print(80*'=')
-            print("TriggerRecordHeader:\t\t", nheader)
-            print('Path:\t\t', name)
-            print('Size:\t\t', dset.shape)
-            print('Data type:\t\t', dset.dtype)
-            nheader += 1
-            data_array = bytearray(dset[:])
-            print_trh(data_array)
-        if not printTRH and "TriggerRecordHeader" not in name:
-            print(80*'=')
-            print("Fragment:\t\t", nheader)
-            print('Path:\t\t', name)
-            print('Size:\t\t', dset.shape)
-            print('Data type:\t\t', dset.dtype)
-            nheader += 1
-            data_array = bytearray(dset[:])
-            print_frag_header(data_array)
+
+def print_fragment_header(data_array):
+    keys = ['Magic word', 'Version', 'Frag Size', 'Trig number',
+        'Trig timestamp', 'Window begin (ticks@50MHz)',
+        'Window end (ticks@50MHz)', 'Run number',
+        'GeoID (APA)', 'GeoID (link)', 'Error bits', 'Fragment type' ]
+    unpack_string = '<2I5Q5I'
+    print_header(unpack_header(data_array[:68], unpack_string, keys))
     return
+
+
+def print_trigger_record_header(data_array):
+    keys = ['Magic word', 'Version', 'Trigger number',
+            'Trigger Timestamp', 'No. of requested components', 'Run Number',
+            'Error bits', 'Trigger type' ]
+    unpack_string = '<2I3Q2IH'
+    print_header(unpack_header(data_array[:42], unpack_string, keys))
+
+    if g_list_components:
+        comp_keys = ['GeoID (APA)', 'GeoID (link)', 'Begin time (ticks@50MHz)'
+                     'End time (ticks@50MHz)']
+        comp_unpack_string = "<2I2Q"
+        for i_values in struct.iter_unpack(comp_unpack_string, data_array[48:]):
+            i_comp = dict(zip(comp_keys, i_values))
+            print_header(i_comp)
+    return
+
+
+def get_header_func(name, dset):
+    global g_n_printed
+    if isinstance(dset, h5py.Dataset):
+        if g_n_request <= 0 or g_n_printed < g_n_request:
+            g_n_printed += 1
+            data_array = bytearray(dset[:])
+            if "TriggerRecordHeader" in name:
+                if g_header_type in ['trigger', 'both']:
+                    print(80*'=')
+                    print('{:<30}:\t{}'.format("Path", name))
+                    print('{:<30}:\t{}'.format("Size", dset.shape))
+                    print('{:<30}:\t{}'.format("Data type", dset.dtype))
+                    print_trigger_record_header(data_array)
+            else:
+                if g_header_type in ['fragment', 'both']:
+                    print(80*'=')
+                    print('{:<30}:\t{}'.format("Path", name))
+                    print('{:<30}:\t{}'.format("Size", dset.shape))
+                    print('{:<30}:\t{}'.format("Data type", dset.dtype))
+                    print_fragment_header(data_array)
+    return
+
 
 def get_header(file_name):
     with h5py.File(file_name, 'r') as f:
-        f.visititems(print_header_func)
+        f.visititems(get_header_func)
     return
 
-def examine(file_name):
-    global nheader
-    nheader = -1
+
+def examine_fragments_func(name, dset):
+    global g_ith_record
+    if isinstance(dset, h5py.Group) and "/" not in name:
+        # This is a new trigger record.
+        g_trigger_record_nfragments.append(0)
+        g_ith_record += 1
+    if isinstance(dset, h5py.Dataset):
+            if "FELIX" in name:
+                # This is a new fragment
+                g_trigger_record_nfragments[g_ith_record] += 1
+            if "TriggerRecordHeader" in name:
+                # This is a new TriggerRecordHeader
+                data_array = bytearray(dset[:])
+                (i,) = struct.unpack('<Q', data_array[24:32])
+                g_trigger_record_nexp_fragments.append(i)
+    return
+
+
+def examine_fragments(file_name):
     with h5py.File(file_name, 'r') as f:
-        f.visititems(examine_fragments)
-    for i in range(len(trigger_record_fragments)):
-        print("Trigger record {} No. of fragments (exp. vs. act): {} vs. {}; diff: {}".format(
-            i, trigger_record_header_expected_fragments[i], trigger_record_fragments[i],
-        trigger_record_header_expected_fragments[i] - trigger_record_fragments[i]
-        ))
+        f.visititems(examine_fragments_func)
+
+    print("{:-^60}".format("Column Definitions"))
+    print("i:           Trigger record number;")
+    print("N_frag_exp:  expected no. of fragments stored in header;")
+    print("N_frag_act:  no. of fragments written in trigger record;")
+    print("N_diff:      N_frag_act - N_frag_exp")
+    print("{:-^60}".format("Column Definitions"))
+    print("{:^10}{:^10}{:^10}{:^10}".format(
+        "i", "N_frag_exp","N_frag_act", "N_diff"))
+    for i in range(len(g_trigger_record_nfragments)):
+        print("{:^10}{:^10}{:^10}{:^10}".format(
+            i, g_trigger_record_nexp_fragments[i],
+            g_trigger_record_nfragments[i],
+         g_trigger_record_nfragments[i] - g_trigger_record_nexp_fragments[i]))
     return
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Python script to parse DUNE-DAQ HDF5 output files.')
-    parser.version = '0.1'
-    parser.add_argument('-p', '--path',
-                       help='Path to HDF5 file',
-                       default='./')
+    parser = argparse.ArgumentParser(
+        description='Python script to parse DUNE-DAQ HDF5 output files.')
 
     parser.add_argument('-f', '--file_name',
-                       help='Name of HDF5 file',
-                       required=True)
+                        help='Path to HDF5 file',
+                        required=True)
 
-    parser.add_argument('-H', '--header_only',
-                       help='Print the header only; no data is displayed',
-                       action='store_true')
+    parser.add_argument('--header', choices=['trigger','fragment', 'both'],
+                        default='both',
+                        help='Select which header data to display')
 
-    parser.add_argument('-TRH', '--TRH_only',
-                       help='Print the TriggerRecordheader only; data is displayed',
-                       action='store_true')
-
-    parser.add_argument('--check-fragment',
-                       help='check if fragments written in trigger record matches expected number in trigger record header',
-                       action='store_true')
+    parser.add_argument('--check-fragments',
+                        help='''check if fragments written in trigger record
+                        matches expected number in trigger record header''',
+                        action='store_true')
 
     parser.add_argument('--list-components',
-                       help='list components in trigger record header',
-                       action='store_true')
+                        help='list components in trigger record header',
+                        action='store_true')
 
-    parser.add_argument('-num', '--num_req_trig_records',
-                        type=int,
-                       help='Select number of trigger records to be parsed',
-                       default=None)
+    parser.add_argument('-n', '--num-of-records', type=int,
+                        help='Select number of records to be parsed',
+                        default=0)
 
-    parser.add_argument('-v', '--version', action='version')
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s 1.0')
     return parser.parse_args()
 
 
-nheader = 0
-NHEADER = 0
-printTRH = False
-listComponents = False
 def main():
     args=parse_args()
 
-    input_path = args.path
-    input_file_name = args.file_name
-    full_path = input_path + input_file_name
-    print("Reading file", full_path )
+    h5file = args.file_name
+    print("Reading file", h5file)
 
-    requested_trigger_records = args.num_req_trig_records
-    global NHEADER
-    global printTRH
-    global listComponents
-    NHEADER = args.num_req_trig_records
+    global g_n_request
+    global g_header_type
+    global g_list_components
 
-    if args.header_only:
-      get_header(full_path)
-    if args.TRH_only:
-      printTRH = True
-      if args.list_components:
-          listComponents = True
-      get_header(full_path)
-    if args.check_fragment:
-      examine(full_path)
+    g_n_request = args.num_of_records
+    g_header_type = args.header
+    g_list_components = args.list_components
+
+
+    if not args.check_fragments:
+        get_header(h5file)
+    else:
+        examine_fragments(h5file)
 
 if __name__ == "__main__":
     main()
