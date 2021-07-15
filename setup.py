dcc46254c4039ee0e7d0ad36cd4f9f72ef2f9647
Kurt Biery <biery@fnal.gov>
2021-07-15 14:42:30 -0500

    Not sure if these setup files are needed for the dfmodules.data_file_checks python module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="dfmodules",
+    install_requires=[
+        "pytest",
+    ],
+)
