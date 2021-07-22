5e506282a3e605b8fff42f0b9b20e1ba34eb17be
Kurt Biery <biery@fnal.gov>
2021-07-22 10:43:07 -0500

    Removed unneeded files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="dfmodules",
-    install_requires=[
-        "pytest",
-    ],
-)
