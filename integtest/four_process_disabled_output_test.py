17aeb406848653c1aaeff0f44e34ce25da1e1188
Kurt Biery <biery@fnal.gov>
2021-08-19 10:23:30 -0500

    Added checking of different readout types; dropped test with DQM enabled since that wasn't adding much to what other tests were doing

--- integtest/four_process_disabled_output_test.py
+++ integtest/four_process_disabled_output_test.py
@@ -9,10 +9,23 @@ run_duration=20  # seconds
 
 # Default values for validation parameters
 expected_number_of_data_files=2
-expected_fragments_per_trigger_record=number_of_data_producers
-min_fragment_size_bytes=37200
-max_fragment_size_bytes=37200
 check_for_logfile_errors=True
+wib1_frag_hsi_trig_params={"fragment_type_description": "WIB", "hdf5_groups": "TPC/APA000",
+                           "element_name_prefix": "Link", "element_number_offset": 0,
+                           "expected_fragment_count": number_of_data_producers,
+                           "min_size_bytes": 37200, "max_size_bytes": 37200}
+wib1_frag_multi_trig_params={"fragment_type_description": "WIB", "hdf5_groups": "TPC/APA000",
+                             "element_name_prefix": "Link", "element_number_offset": 0,
+                             "expected_fragment_count": number_of_data_producers,
+                             "min_size_bytes": 1000, "max_size_bytes": 37200}
+rawtp_frag_params={"fragment_type_description": "Raw TP", "hdf5_groups": "TPC/TP_APA000",
+                   "element_name_prefix": "Link", "element_number_offset": number_of_data_producers,
+                   "expected_fragment_count": number_of_data_producers,
+                   "min_size_bytes": 80, "max_size_bytes": 80}
+triggertp_frag_params={"fragment_type_description": "Trigger TP", "hdf5_groups": "Trigger/Region000",
+                       "element_name_prefix": "Element", "element_number_offset": 0,
+                       "expected_fragment_count": number_of_data_producers,
+                       "min_size_bytes": 80, "max_size_bytes": 80}
 
 # The next three variable declarations *must* be present as globals in the test
 # file. They're read by the "fixtures" in conftest.py to determine how
@@ -23,7 +36,8 @@ confgen_name="minidaqapp.nanorc.mdapp_multiru_gen"
 # The arguments to pass to the config generator, excluding the json
 # output directory (the test framework handles that)
 confgen_arguments_base=[ "-d", "./frames.bin", "-o", ".", "-s", "10", "-n", str(number_of_data_producers), "-b", "1000", "-a", "1000", "--host-ru", "localhost"]
-confgen_arguments=[ confgen_arguments_base, confgen_arguments_base+["--enable-software-tpg"], confgen_arguments_base+["--enable-dqm"] ]
+confgen_arguments={"WIB1_System": confgen_arguments_base,
+                   "Software_TPG_System": confgen_arguments_base+["--enable-software-tpg"]}
 # The commands to run in nanorc, as a list
 nanorc_command_list="boot init conf".split()
 nanorc_command_list+="start --disable-data-storage 101 wait ".split() + [str(run_duration)] + "stop --stop-wait 2 wait 2".split()
@@ -39,21 +53,18 @@ def test_nanorc_success(run_nanorc):
     assert run_nanorc.completed_process.returncode==0
 
 def test_log_files(run_nanorc):
-    local_check_flag=check_for_logfile_errors
-    if "--enable-dqm" in run_nanorc.confgen_arguments:
-        local_check_flag=False
-
-    if local_check_flag:
+    if check_for_logfile_errors:
         # Check that there are no warnings or errors in the log files
         assert log_file_checks.logs_are_error_free(run_nanorc.log_files)
 
 def test_data_file(run_nanorc):
-    local_expected_frags=expected_fragments_per_trigger_record
-    local_min_frag_size=min_fragment_size_bytes
-    local_max_frag_size=max_fragment_size_bytes
+    fragment_check_list=[]
     if "--enable-software-tpg" in run_nanorc.confgen_arguments:
-        local_expected_frags*=2
-        local_min_frag_size=80
+        fragment_check_list.append(wib1_frag_multi_trig_params)
+        fragment_check_list.append(rawtp_frag_params)
+        fragment_check_list.append(triggertp_frag_params)
+    if len(fragment_check_list) == 0:
+        fragment_check_list.append(wib1_frag_hsi_trig_params)
 
     # Run some tests on the output data file
     assert len(run_nanorc.data_files)==expected_number_of_data_files
@@ -61,5 +72,6 @@ def test_data_file(run_nanorc):
     for idx in range(len(run_nanorc.data_files)):
         data_file=data_file_checks.DataFile(run_nanorc.data_files[idx])
         assert data_file_checks.sanity_check(data_file)
-        assert data_file_checks.check_link_presence(data_file, n_links=local_expected_frags)
-        assert data_file_checks.check_fragment_sizes(data_file, min_frag_size=local_min_frag_size, max_frag_size=local_max_frag_size)
+        for jdx in range(len(fragment_check_list)):
+            assert data_file_checks.check_fragment_presence(data_file, fragment_check_list[jdx])
+            assert data_file_checks.check_fragment_size2(data_file, fragment_check_list[jdx])
