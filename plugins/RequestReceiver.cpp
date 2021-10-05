952c192a9c77d4c4ec57d16b2fa4cb7a8895ce8b
Kurt Biery <biery@fnal.gov>
2021-10-04 21:20:14 -0500

    Remove const-ness of the dispatch_request method because it increments a data member

--- plugins/RequestReceiver.cpp
+++ plugins/RequestReceiver.cpp
@@ -140,7 +140,7 @@ RequestReceiver::get_info(opmonlib::InfoCollector& ci, int /*level*/)
 }
 
 void
-RequestReceiver::dispatch_request(ipm::Receiver::Response message) const
+RequestReceiver::dispatch_request(ipm::Receiver::Response message)
 {
   auto request = serialization::deserialize<dfmessages::DataRequest>(message.data);
 
