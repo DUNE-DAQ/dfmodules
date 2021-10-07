d2a85e8ffe1f7b34d07c965e1e31185c70ff59a7
Kurt Biery <biery@fnal.gov>
2021-10-07 12:46:51 -0500

    Changes to allow all HDF5 data file parameters to be set via configuration

--- plugins/HDF5KeyTranslator.hpp
+++ plugins/HDF5KeyTranslator.hpp
@@ -13,7 +13,6 @@
 #ifndef DFMODULES_PLUGINS_HDF5KEYTRANSLATOR_HPP_
 #define DFMODULES_PLUGINS_HDF5KEYTRANSLATOR_HPP_
 
-#include "dfmodules/HDF5FormattingParameters.hpp"
 #include "dfmodules/StorageKey.hpp"
 #include "dfmodules/hdf5datastore/Nljs.hpp"
 #include "dfmodules/hdf5datastore/Structs.hpp"
@@ -26,6 +25,8 @@
 #include <string>
 #include <vector>
 
+namespace hdf5ds = dunedaq::dfmodules::hdf5datastore;
+
 namespace dunedaq {
 namespace dfmodules {
 
@@ -35,10 +36,36 @@ class HDF5KeyTranslator
 public:
   inline static const std::string path_separator = "/";
 
-  HDF5KeyTranslator(/*OperationalEnvironmentType op_env_type*/)
+  struct DataRecordParameters
   {
-    m_data_record_params = HDF5FormattingParameters::get_data_record_parameters(/*op_env_type,*/ get_current_version());
-    m_path_param_map = HDF5FormattingParameters::get_path_parameters(/*op_env_type,*/ get_current_version());
+    std::string trigger_record_name_prefix;
+    int digits_for_trigger_number;
+  };
+
+  HDF5KeyTranslator(const hdf5datastore::ConfParams& config_params)
+  {
+    m_data_record_params.trigger_record_name_prefix = config_params.file_layout_parameters.trigger_record_name_prefix;
+    m_data_record_params.digits_for_trigger_number = config_params.file_layout_parameters.digits_for_trigger_number;
+
+    m_current_version = config_params.version;
+
+    hdf5ds::PathParamList path_param_list = config_params.file_layout_parameters.path_param_list;
+    for (const hdf5ds::PathParams& path_param_set : path_param_list) {
+      if (path_param_set.detector_group_type == "TPC") {
+        m_path_param_map[StorageKey::DataRecordGroupType::kTPC] = path_param_set;
+      }
+      else if (path_param_set.detector_group_type == "PDS") {
+        m_path_param_map[StorageKey::DataRecordGroupType::kPDS] = path_param_set;
+      }
+      else if (path_param_set.detector_group_type == "Trigger") {
+        m_path_param_map[StorageKey::DataRecordGroupType::kTrigger] = path_param_set;
+      }
+      else if (path_param_set.detector_group_type == "TPC_TP") {
+        m_path_param_map[StorageKey::DataRecordGroupType::kTPC_TP] = path_param_set;
+      }
+    }
+
+    m_config_params = config_params;
   }
 
   /**
@@ -82,7 +109,7 @@ public:
 
     if (data_key.get_group_type() != StorageKey::DataRecordGroupType::kTriggerRecordHeader) {
       // Add group type
-      path_list.push_back(m_path_param_map[data_key.get_group_type()].group_name_within_data_record);
+      path_list.push_back(m_path_param_map[data_key.get_group_type()].detector_group_name);
 
       // next, we translate the region number
       std::ostringstream region_number_string;
@@ -107,24 +134,22 @@ public:
 
   /**
    * @brief Returns the version number of the HDF5 paths that are currently being
-   * returned by this class. This is independent of the translations from HDF5 paths
-   * to StorageKeys (that translation may support multiple versions).
+   * returned by this class.
    */
-  int get_current_version() { return HDF5FormattingParameters::get_current_version(); }
+  int get_current_version() { return m_current_version; }
 
   /**
    * @brief Translates the specified input parameters into the appropriate filename.
    */
   std::string get_file_name(const StorageKey& data_key,
-                            const hdf5datastore::ConfParams& config_params,
                             size_t file_index)
   {
     std::ostringstream work_oss;
-    work_oss << config_params.directory_path;
+    work_oss << m_config_params.directory_path;
     if (work_oss.str().length() > 0) {
       work_oss << "/";
     }
-    work_oss << config_params.filename_parameters.overall_prefix;
+    work_oss << m_config_params.filename_parameters.overall_prefix;
     if (work_oss.str().length() > 0) {
       work_oss << "_";
     }
@@ -132,32 +157,32 @@ public:
     size_t trigger_number = data_key.get_trigger_number();
     size_t region_number = data_key.get_region_number();
     std::string file_name = std::string("");
-    if (config_params.mode == "one-event-per-file") {
+    if (m_config_params.mode == "one-event-per-file") {
 
-      file_name = config_params.directory_path + "/" + config_params.filename_parameters.overall_prefix +
+      file_name = m_config_params.directory_path + "/" + m_config_params.filename_parameters.overall_prefix +
                   "_trigger_number_" + std::to_string(trigger_number) + ".hdf5";
       return file_name;
 
-    } else if (config_params.mode == "one-fragment-per-file") {
+    } else if (m_config_params.mode == "one-fragment-per-file") {
 
-      file_name = config_params.directory_path + "/" + config_params.filename_parameters.overall_prefix +
+      file_name = m_config_params.directory_path + "/" + m_config_params.filename_parameters.overall_prefix +
                   "_trigger_number_" + std::to_string(trigger_number) + "_region_number_" +
                   std::to_string(region_number) + ".hdf5";
       return file_name;
 
-    } else if (config_params.mode == "all-per-file") {
+    } else if (m_config_params.mode == "all-per-file") {
 
-      // file_name = config_params.directory_path + "/" + config_params.filename_parameters.overall_prefix +
-      // "_all_events" + ".hdf5"; file_name = config_params.directory_path + "/" +
-      // config_params.filename_parameters.overall_prefix + "_trigger_number_" + "file_number_" +
+      // file_name = m_config_params.directory_path + "/" + m_config_params.filename_parameters.overall_prefix +
+      // "_all_events" + ".hdf5"; file_name = m_config_params.directory_path + "/" +
+      // m_config_params.filename_parameters.overall_prefix + "_trigger_number_" + "file_number_" +
       // std::to_string(file_index) + ".hdf5";
 
-      work_oss << config_params.filename_parameters.run_number_prefix;
-      work_oss << std::setw(config_params.filename_parameters.digits_for_run_number) << std::setfill('0')
+      work_oss << m_config_params.filename_parameters.run_number_prefix;
+      work_oss << std::setw(m_config_params.filename_parameters.digits_for_run_number) << std::setfill('0')
                << data_key.get_run_number();
       work_oss << "_";
-      work_oss << config_params.filename_parameters.file_index_prefix;
-      work_oss << std::setw(config_params.filename_parameters.digits_for_file_index) << std::setfill('0') << file_index;
+      work_oss << m_config_params.filename_parameters.file_index_prefix;
+      work_oss << std::setw(m_config_params.filename_parameters.digits_for_file_index) << std::setfill('0') << file_index;
     }
 
     work_oss << ".hdf5";
@@ -165,9 +190,10 @@ public:
   }
 
 private:
-  static constexpr int s_current_version = 1;
-  HDF5FormattingParameters::DataRecordParameters m_data_record_params;
-  std::map<StorageKey::DataRecordGroupType, HDF5FormattingParameters::PathParameters> m_path_param_map;
+  int m_current_version = 1;
+  DataRecordParameters m_data_record_params;
+  hdf5datastore::ConfParams m_config_params;
+  std::map<StorageKey::DataRecordGroupType, hdf5ds::PathParams> m_path_param_map;
 };
 
 } // namespace dfmodules
--- schema/dfmodules/hdf5datastore.jsonnet
+++ schema/dfmodules/hdf5datastore.jsonnet
@@ -3,64 +3,66 @@ local ns = "dunedaq.dfmodules.hdf5datastore";
 local s = moo.oschema.schema(ns);
 
 local types = {
-    size: s.number("Size", "u8",
-                   doc="A count of very many things"),
+    size : s.number("Size", "u8", doc="A count of very many things"),
 
-    count : s.number("Count", "i4",
-                     doc="A count of not too many things"),
+    count : s.number("Count", "i4", doc="A count of not too many things"),
 
     factor : s.number("Factor", "f4", doc="A float number of 4 bytes"),
 
-    data_store_type: s.string( "DataStoreType", doc="Specific Data store implementation to be instantiated" ),
-
-    data_store_name: s.string( "DataStoreName", doc="String to specify names for DataStores"),
-
-    opmode: s.string("OperationMode", doc="String used to specify a data storage operation mode"),
-
-    dirpath: s.string("DirectoryPath", doc="String used to specify a directory path"),
-
-    fnprefix: s.string("FilenamePrefix", doc="String used to specify a filename prefix"),
-
-    flprefix: s.string("FileLayoutPrefix", doc="String used to specify a Group or DataSet name prefix"),
+    ds_string : s.string("DataStoreString", doc="A string used in the data store configuration"),
 
     flag: s.boolean("Flag", doc="Parameter that can be used to enable or disable functionality"),
 
-    hdf5_filename_params: s.record("HDF5DataStoreFileNameParams", [
-        s.field("overall_prefix", self.fnprefix, "fake_minidaqapp",
+    hdf5_filename_params: s.record("FileNameParams", [
+        s.field("overall_prefix", self.ds_string, "minidaq",
                 doc="Prefix for the overall filename for the files on disk"),
-        s.field("run_number_prefix", self.fnprefix, "run",
+        s.field("run_number_prefix", self.ds_string, "run",
                 doc="Prefix for the run number part of the filename"),
         s.field("digits_for_run_number", self.count, 6,
                 doc="Number of digits to use for the run number when formatting the filename"),
-        s.field("file_index_prefix", self.fnprefix, "",
+        s.field("file_index_prefix", self.ds_string, "",
                 doc="Prefix for the file index part of the filename"),
         s.field("digits_for_file_index", self.count, 4,
                 doc="Number of digits to use for the file index when formatting the filename"),
     ], doc="Parameters for the HDF5DataStore filenames"),
 
-    hdf5_file_layout_params: s.record("HDF5DataStoreFileLayoutParams", [
-        s.field("trigger_record_name_prefix", self.flprefix, "TriggerRecord",
+    subdet_path_params : s.record("PathParams", [
+           s.field("detector_group_type", self.ds_string, "unspecified",
+                   doc="The special keyword that identifies this entry (e.g. \"TPC\", \"PDS\", \"TPC_TP\", etc.)"),
+           s.field("detector_group_name", self.ds_string, "unspecified",
+                   doc="The detector name that should be used inside the HDF5 file"),
+           s.field("region_name_prefix", self.ds_string, "Region",
+                   doc="Prefix for the Region name"),
+           s.field("digits_for_region_number", self.count, 3,
+                   doc="Number of digits to use for the region number inside the HDF5 file"),
+           s.field("element_name_prefix", self.ds_string, "Element",
+                   doc="Prefix for the Element name"),
+           s.field("digits_for_element_number", self.count, 2,
+                   doc="Number of digits to use for the Element number inside the HDF5 file") ],
+        doc="Parameters for the HDF5 Group and DataSet names"),
+
+    list_of_path_params : s.sequence("PathParamList", self.subdet_path_params, doc="List of subdetector path parameters" ),
+
+    hdf5_file_layout_params: s.record("FileLayoutParams", [
+        s.field("trigger_record_name_prefix", self.ds_string, "TriggerRecord",
                 doc="Prefix for the TriggerRecord name"),
         s.field("digits_for_trigger_number", self.count, 6,
                 doc="Number of digits to use for the TriggerRecord name inside the HDF5 file"),
-        s.field("apa_name_prefix", self.flprefix, "APA",
-                doc="Prefix for the APA name"),
-        s.field("digits_for_apa_number", self.count, 3,
-                doc="Number of digits to use for the APA number inside the HDF5 file"),
-        s.field("link_name_prefix", self.flprefix, "Link",
-                doc="Prefix for the Link name"),
-        s.field("digits_for_link_number", self.count, 2,
-                doc="Number of digits to use for the Link number inside the HDF5 file"),
+        s.field("path_param_list", self.list_of_path_params, doc=""),
     ], doc="Parameters for the layout of Groups and DataSets within the HDF5 file"),
 
     conf: s.record("ConfParams", [
-        s.field("type", self.data_store_type, "HDF5DataStore", 
-                 doc="DataStore specific implementation" ), 
-        s.field("name", self.data_store_name, "store", 
-                 doc="DataStore name" ), 
-        s.field("mode", self.opmode, "all-per-file",
+        s.field("type", self.ds_string, "HDF5DataStore",
+                 doc="DataStore specific implementation"),
+        s.field("name", self.ds_string, "store",
+                 doc="DataStore name"),
+        s.field("version", self.count, 1,
+                 doc="Version of the HDF5DataStore parameters (e.g. Group and DataSet names)"),
+        s.field("operational_environment", self.ds_string, "minidaq",
+                 doc="Operational environment (where the DAQ is running, e.g. \"coldbox\")"),
+        s.field("mode", self.ds_string, "all-per-file",
                 doc="The operation mode that the DataStore should use when organizing the data into files"),
-        s.field("directory_path", self.dirpath, ".",
+        s.field("directory_path", self.ds_string, ".",
                 doc="Path of directory where files are located"),
         s.field("max_file_size_bytes", self.size, 1048576,
                 doc="Maximum number of bytes in each raw data file"),
