203b056c3b47831fa836f639ce1d1840588af948
Eric Flumerfelt <eflumerf@fnal.gov>
2021-12-03 15:52:18 -0600

    Add initial implementation of DataflowApplicationData (name?)

---
+++ src/DataflowApplicationData.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file DataflowApplicationData.cpp DataflowApplicationData Class Implementation
+ *
+ * The DataflowApplicationData class represents the current state of a dataflow application's Trigger Record buffers for
+ * use by the DFO.
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "dfmodules/DataflowApplicationData.hpp"
+
+namespace dunedaq {
+namespace dfmodules {
+
+std::shared_ptr<AssignedTriggerDecision>
+DataflowApplicationData::extract_assignment(daqdataformats::trigger_number_t trigger_number)
+{
+  std::shared_ptr<AssignedTriggerDecision> dec_ptr;
+  auto lk = std::lock_guard<std::mutex>(m_assigned_trigger_decisions_mutex);
+  for (auto it = m_assigned_trigger_decisions.begin(); it != m_assigned_trigger_decisions.end(); ++it) {
+    if ((*it)->trigger_number == trigger_number) {
+      dec_ptr = *it;
+      m_assigned_trigger_decisions.erase(it);
+      break;
+    }
+  }
+
+  return dec_ptr;
+}
+
+std::shared_ptr<AssignedTriggerDecision>
+DataflowApplicationData::get_assignment(daqdataformats::trigger_number_t trigger_number) const
+{
+  auto lk = std::lock_guard<std::mutex>(m_assigned_trigger_decisions_mutex);
+  for (auto ptr : m_assigned_trigger_decisions) {
+    if (ptr->trigger_number == trigger_number) {
+      return ptr;
+    }
+  }
+
+  return nullptr;
+}
+
+void
+DataflowApplicationData::complete_assignment(daqdataformats::trigger_number_t trigger_number,
+                                             std::function<void(nlohmann::json&)> metadata_fun)
+{
+
+  auto dec_ptr = extract_assignment(trigger_number);
+
+  if (dec_ptr == nullptr)
+    throw AssignedTriggerDecisionNotFound(ERS_HERE, trigger_number, m_connection_name);
+  auto now = std::chrono::steady_clock::now();
+  m_latency_info.emplace_back(now, std::chrono::duration_cast<std::chrono::microseconds>(now - dec_ptr->assigned_time));
+
+  if (m_latency_info.size() > 1000)
+    m_latency_info.pop_front();
+
+  if (metadata_fun)
+    metadata_fun(m_metadata);
+}
+
+std::shared_ptr<AssignedTriggerDecision>
+DataflowApplicationData::add_assignment(daqdataformats::trigger_number_t trigger_number)
+{
+  auto lk = std::lock_guard<std::mutex>(m_assigned_trigger_decisions_mutex);
+  m_assigned_trigger_decisions.push_back(std::make_shared<AssignedTriggerDecision>(trigger_number, m_connection_name));
+  return m_assigned_trigger_decisions.back();
+}
+
+} // namespace dfmodules
+} // namespace dunedaq
---
+++ src/dfmodules/DataflowApplicationData.hpp
@@ -0,0 +1,75 @@
+/**
+ * @file DataflowApplicationData.hpp DataflowApplicationData Class
+ *
+ * The DataflowApplicationData class represents the current state of a dataflow application's Trigger Record buffers for
+ * use by the DFO.
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef DFMODULES_SRC_DFMODULES_DATAFLOWAPPLICATIONDATA_HPP_
+#define DFMODULES_SRC_DFMODULES_DATAFLOWAPPLICATIONDATA_HPP_
+
+#include "daqdataformats/Types.hpp"
+
+#include "ers/Issue.hpp"
+#include "nlohmann/json.hpp"
+
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <string>
+
+namespace dunedaq {
+ERS_DECLARE_ISSUE(dfmodules,
+                  AssignedTriggerDecisionNotFound,
+                  "The Trigger Decision with trigger number "
+                    << trigger_number << " was not found for dataflow application at " << connection_name,
+                  ((daqdataformats::trigger_number_t)trigger_number)((std::string)connection_name))
+
+namespace dfmodules {
+struct AssignedTriggerDecision
+{
+  daqdataformats::trigger_number_t trigger_number;
+  std::chrono::steady_clock::time_point assigned_time;
+  std::string connection_name;
+
+  AssignedTriggerDecision(daqdataformats::trigger_number_t trig_num, std::string conn_name)
+    : trigger_number(trig_num)
+    , assigned_time(std::chrono::steady_clock::now())
+    , connection_name(conn_name)
+  {}
+};
+
+class DataflowApplicationData
+{
+public:
+  bool has_slot() const { return m_num_slots.load() > m_assigned_trigger_decisions.size(); }
+  size_t available_slots() const { return m_num_slots.load() - m_assigned_trigger_decisions.size(); }
+
+  std::shared_ptr<AssignedTriggerDecision> get_assignment(daqdataformats::trigger_number_t trigger_number) const;
+  std::shared_ptr<AssignedTriggerDecision> extract_assignment(daqdataformats::trigger_number_t trigger_number);
+  std::shared_ptr<AssignedTriggerDecision> add_assignment(daqdataformats::trigger_number_t trigger_number);
+  void complete_assignment(daqdataformats::trigger_number_t trigger_number,
+                           std::function<void(nlohmann::json&)> metadata_fun = nullptr);
+
+private:
+  std::atomic<size_t> m_num_slots;
+  std::list<std::shared_ptr<AssignedTriggerDecision>> m_assigned_trigger_decisions;
+  mutable std::mutex m_assigned_trigger_decisions_mutex;
+
+  // TODO: Eric Flumerfelt <eflumerf@github.com> 03-Dec-2021: Replace with circular buffer
+  std::list<std::pair<std::chrono::steady_clock::time_point, std::chrono::microseconds>> m_latency_info;
+
+  nlohmann::json m_metadata;
+  std::string m_connection_name;
+};
+} // namespace dfmodules
+} // namespace dunedaq
+
+#endif // DFMODULES_SRC_DFMODULES_DATAFLOWAPPLICATIONDATA_HPP_
