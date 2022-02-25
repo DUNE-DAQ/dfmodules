b267353965c1e542ab7b9dfd88d918690deebf46
Kurt Biery <biery@fnal.gov>
2022-02-25 13:34:51 -0600

    updated integtests to use daqconf_multiru_gen

--- integtest/six_process_stop_start_test.py
+++ integtest/six_process_stop_start_test.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 import re
-import psutil
+#import psutil
 
 import dfmodules.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
@@ -42,9 +42,9 @@ ignored_logfile_problems={"dqm": ["client will not be able to connect to Kafka c
 sufficient_resources_on_this_computer=True
 cpu_count=os.cpu_count()
 hostname=os.uname().nodename
-mem_obj=psutil.virtual_memory()
-free_mem=round((mem_obj.available/(1024*1024*1024)), 2)
-total_mem=round((mem_obj.total/(1024*1024*1024)), 2)
+#mem_obj=psutil.virtual_memory()
+free_mem=999  #round((mem_obj.available/(1024*1024*1024)), 2)
+total_mem=999 #round((mem_obj.total/(1024*1024*1024)), 2)
 print(f"DEBUG: CPU count is {cpu_count}, free and total memory are {free_mem} GB and {total_mem} GB.")
 if cpu_count < 18 or free_mem < 24:
     sufficient_resources_on_this_computer=False
@@ -54,7 +54,7 @@ if cpu_count < 18 or free_mem < 24:
 # to run the config generation and nanorc
 
 # The name of the python module for the config generation
-confgen_name="minidaqapp.newconf.mdapp_multiru_gen"
+confgen_name="daqconf_multiru_gen"
 
 # The arguments to pass to the config generator, excluding the json
 # output directory (the test framework handles that)
