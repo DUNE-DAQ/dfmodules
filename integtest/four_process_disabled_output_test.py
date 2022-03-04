e745da1a02e7577455b12b3d02330b2eb87c1cd2
Kurt Biery <biery@fnal.gov>
2022-03-03 21:49:35 -0600

    Updated integtests to account for all of the Readout TP fragments temporarily having a system type of TPC and to account for the non-empty TP fragments coming from Trigger.

--- integtest/four_process_disabled_output_test.py
+++ integtest/four_process_disabled_output_test.py
@@ -20,16 +20,12 @@ wib1_frag_hsi_trig_params={"fragment_type_description": "WIB",
                            "min_size_bytes": 37200, "max_size_bytes": 37200}
 wib1_frag_multi_trig_params={"fragment_type_description": "WIB",
                              "hdf5_detector_group": "TPC", "hdf5_region_prefix": "APA",
-                             "expected_fragment_count": number_of_data_producers,
+                             "expected_fragment_count": number_of_data_producers*2,
                              "min_size_bytes": 80, "max_size_bytes": 37200}
-rawtp_frag_params={"fragment_type_description": "Raw TP",
-                   "hdf5_detector_group": "TPC", "hdf5_region_prefix": "TP_APA",
-                   "expected_fragment_count": number_of_data_producers,
-                   "min_size_bytes": 80, "max_size_bytes": 80}
 triggertp_frag_params={"fragment_type_description": "Trigger TP",
                        "hdf5_detector_group": "Trigger", "hdf5_region_prefix": "Region",
                        "expected_fragment_count": number_of_data_producers,
-                       "min_size_bytes": 80, "max_size_bytes": 80}
+                       "min_size_bytes": 80, "max_size_bytes": 16000}
 
 # The next three variable declarations *must* be present as globals in the test
 # file. They're read by the "fixtures" in conftest.py to determine how
@@ -77,7 +73,6 @@ def test_data_file(run_nanorc):
         local_expected_event_count+=(270*number_of_data_producers*run_duration/100)
         local_event_count_tolerance+=(10*number_of_data_producers*run_duration/100)
         fragment_check_list.append(wib1_frag_multi_trig_params)
-        fragment_check_list.append(rawtp_frag_params)
         fragment_check_list.append(triggertp_frag_params)
     if len(fragment_check_list) == 0:
         fragment_check_list.append(wib1_frag_hsi_trig_params)
