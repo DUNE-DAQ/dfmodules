bf78ddbfe2464ab0b79f4de54916d0b9402981a5
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2022-03-05 01:41:50 -0600

    add optional argument for printing component details in print_trigger_record_header

--- scripts/hdf5dump/binary_dump.py
+++ scripts/hdf5dump/binary_dump.py
@@ -54,7 +54,7 @@ def read_binary_file(fname):
                 nfrag = 0
                 if ntrh !=0 and g_header_type in ['both', 'fragment']:
                     print(80*'-')
-                    print_fragment_header(bytesbuffer, g_list_components)
+                    print_fragment_header(bytesbuffer)
                 bytesbuffer = bytearray()
                 ntrh += 1
                 if ntrh > g_n_request and g_n_request != 0: break
