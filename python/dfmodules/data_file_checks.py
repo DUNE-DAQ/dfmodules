120f92db98d45e0925fc7352695fe93b6daced9e
Kurt Biery <biery@fnal.gov>
2022-04-15 13:22:26 -0500

    Another attempt to fix the integtest data-file creation time attribute test

--- python/dfmodules/data_file_checks.py
+++ python/dfmodules/data_file_checks.py
@@ -74,18 +74,19 @@ def check_file_attributes(datafile):
                     print(f"The value in Attribute '{expected_attr_name}' ({attr_value}) does not match the value in the filename ({base_filename})")
         elif expected_attr_name == "creation_timestamp":
             attr_value = datafile.h5file.attrs.get(expected_attr_name)
-            date_obj = datetime.datetime.fromtimestamp((int(attr_value)-1) / 1000, datetime.timezone.utc)
+            date_obj = datetime.datetime.fromtimestamp((int(attr_value)/1000)-1, datetime.timezone.utc)
             date_string = date_obj.strftime("%Y%m%dT%H%M%S")
             pattern_low = f".*{date_string}.*"
-            date_obj = datetime.datetime.fromtimestamp((int(attr_value)+1) / 1000, datetime.timezone.utc)
+            date_obj = datetime.datetime.fromtimestamp((int(attr_value)/1000)+1, datetime.timezone.utc)
             date_string = date_obj.strftime("%Y%m%dT%H%M%S")
             pattern_high = f".*{date_string}.*"
-            date_obj = datetime.datetime.fromtimestamp((int(attr_value)+0) / 1000, datetime.timezone.utc)
+            date_obj = datetime.datetime.fromtimestamp((int(attr_value)/1000)+0, datetime.timezone.utc)
             date_string = date_obj.strftime("%Y%m%dT%H%M%S")
             pattern_exact = f".*{date_string}.*"
             if not re.match(pattern_exact, base_filename) and not re.match(pattern_low, base_filename) and not re.match(pattern_high, base_filename):
                 passed=False
                 print(f"The value in Attribute '{expected_attr_name}' ({date_string}) does not match the value in the filename ({base_filename})")
+                print(f"Debug information: pattern_low={pattern_low} pattern_high={pattern_high} pattern_exact={pattern_exact}")
     if passed:
         print(f"All Attribute tests passed for file {base_filename}")
     return passed
