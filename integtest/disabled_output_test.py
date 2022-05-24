23b8fe68bb568b7ec1fd1696fa9c7f2c7b7cc2bf
Kurt Biery <biery@fnal.gov>
2022-05-24 15:07:02 -0500

    Updated the expected number of fragments for the SWTPG case in a couple of integtests

--- integtest/disabled_output_test.py
+++ integtest/disabled_output_test.py
@@ -28,7 +28,7 @@ triggercandidate_frag_params={"fragment_type_description": "Trigger Candidate",
                               "min_size_bytes": 130, "max_size_bytes": 150}
 triggertp_frag_params={"fragment_type_description": "Trigger with TPs",
                        "hdf5_detector_group": "Trigger", "hdf5_region_prefix": "Region",
-                       "expected_fragment_count": number_of_data_producers,
+                       "expected_fragment_count": number_of_data_producers+2,
                        "min_size_bytes": 80, "max_size_bytes": 16000}
 
 # The next three variable declarations *must* be present as globals in the test
