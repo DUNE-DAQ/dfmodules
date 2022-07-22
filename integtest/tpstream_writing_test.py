c06b4e602e2c28ae573c71bba147fa2b829061ac
Eric Flumerfelt <eflumerf@fnal.gov>
2022-07-22 08:18:26 -0500

    Update command order in tpstream_writing_test. Add test method which gathers and tests tpstream output files.

--- integtest/tpstream_writing_test.py
+++ integtest/tpstream_writing_test.py
@@ -13,7 +13,7 @@ pulser_trigger_rate=1.0 # Hz
 run_duration=30  # seconds
 
 # Default values for validation parameters
-expected_number_of_data_files=3*number_of_dataflow_apps
+expected_number_of_data_files=2*number_of_dataflow_apps
 check_for_logfile_errors=True
 expected_event_count=run_duration*pulser_trigger_rate/number_of_dataflow_apps
 expected_event_count_tolerance=expected_event_count/10
@@ -25,6 +25,10 @@ wib1_frag_multi_trig_params={"fragment_type_description": "WIB",
                              "hdf5_detector_group": "TPC", "hdf5_region_prefix": "APA",
                              "expected_fragment_count": (number_of_data_producers*number_of_readout_apps),
                              "min_size_bytes": 80, "max_size_bytes": 37200}
+wib1_tpset_params={"fragment_type_description": "TP Stream",
+                             "hdf5_detector_group": "TPC", "hdf5_region_prefix": "APA",
+                             "expected_fragment_count": (number_of_data_producers*number_of_readout_apps),
+                             "min_size_bytes": 80, "max_size_bytes": 2800000}
 triggercandidate_frag_params={"fragment_type_description": "Trigger Candidate",
                               "hdf5_detector_group": "Trigger", "hdf5_region_prefix": "Region",
                               "expected_fragment_count": 1,
@@ -43,15 +47,16 @@ ignored_logfile_problems={"dqm": ["client will not be able to connect to Kafka c
 confgen_name="daqconf_multiru_gen"
 # The arguments to pass to the config generator, excluding the json
 # output directory (the test framework handles that)
-confgen_arguments_base=[ "-d", "./frames.bin", "-o", ".", "-s", "10", "-n", str(number_of_data_producers), "-b", "1000", "-a", "1000", "-t", str(pulser_trigger_rate), "--latency-buffer-size", "200000"] + [ "--host-ru", "localhost" ] * number_of_readout_apps + [ "--host-df", "localhost" ] * number_of_dataflow_apps
+output_dir="."
+confgen_arguments_base=[ "-d", "./frames.bin", "-o", output_dir, "-s", "10", "-n", str(number_of_data_producers), "-b", "1000", "-a", "1000", "-t", str(pulser_trigger_rate), "--latency-buffer-size", "200000"] + [ "--host-ru", "localhost" ] * number_of_readout_apps + [ "--host-df", "localhost" ] * number_of_dataflow_apps
 for idx in range(number_of_readout_apps):
     confgen_arguments_base+=["--region-id", str(idx)] 
 confgen_arguments={"Software_TPG_System": confgen_arguments_base+["--enable-software-tpg", "--enable-tpset-writing", "-c", str(3*number_of_data_producers*number_of_readout_apps)],
                   }
 # The commands to run in nanorc, as a list
-nanorc_command_list="integtest-partition boot init conf".split()
-nanorc_command_list+="start --resume-wait 2 101 wait ".split() + [str(run_duration)] + "stop               wait 2".split()
-nanorc_command_list+="start                 102 wait ".split() + [str(run_duration)] + "stop --stop-wait 2 wait 2".split()
+nanorc_command_list="integtest-partition boot conf".split()
+nanorc_command_list+="start_run --wait 2 101 wait ".split() + [str(run_duration)] + "stop_run          wait 2".split()
+nanorc_command_list+="start_run          102 wait ".split() + [str(run_duration)] + "stop_run --wait 2 wait 2".split()
 nanorc_command_list+="scrap terminate".split()
 
 # The tests themselves
@@ -102,3 +107,22 @@ def test_data_files(run_nanorc):
         for jdx in range(len(fragment_check_list)):
             assert data_file_checks.check_fragment_count(data_file, fragment_check_list[jdx])
             assert data_file_checks.check_fragment_sizes(data_file, fragment_check_list[jdx])
+
+def test_tpstream_files(run_nanorc):
+    data_dir=output_dir if output_dir != "." and output_dir != "" else run_nanorc.run_dir
+    tpstream_files = list(data_dir.glob("tpstream_*.hdf5"))
+    local_expected_event_count=run_duration # TPStreamWriter is currently configured to write at 1 Hz
+    local_event_count_tolerance = local_expected_event_count / 10
+    fragment_check_list=[wib1_tpset_params]
+
+    assert len(tpstream_files) == 2 # one for each run
+
+    for idx in range(len(tpstream_files)):
+        data_file=data_file_checks.DataFile(tpstream_files[idx])
+        #assert data_file_checks.sanity_check(data_file) # Sanity check doesn't work for stream files
+        assert data_file_checks.check_file_attributes(data_file)
+        assert data_file_checks.check_event_count(data_file, local_expected_event_count, local_event_count_tolerance)
+        for jdx in range(len(fragment_check_list)):
+            assert data_file_checks.check_fragment_count(data_file, fragment_check_list[jdx])
+            assert data_file_checks.check_fragment_sizes(data_file, fragment_check_list[jdx])
+
