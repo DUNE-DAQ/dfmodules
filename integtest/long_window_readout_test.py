00507ca26619c9f680b098292cd552a179b6da59
Eric Flumerfelt <eflumerf@fnal.gov>
2023-05-03 15:35:40 -0400

    Only expect 2 files

--- integtest/long_window_readout_test.py
+++ integtest/long_window_readout_test.py
@@ -26,7 +26,7 @@ latency_buffer_size=600000
 data_rate_slowdown_factor=20
 
 # Default values for validation parameters
-expected_number_of_data_files=4
+expected_number_of_data_files=2
 check_for_logfile_errors=True
 expected_event_count=191 # 3*run_duration*trigger_rate/number_of_dataflow_apps
 expected_event_count_tolerance=5
