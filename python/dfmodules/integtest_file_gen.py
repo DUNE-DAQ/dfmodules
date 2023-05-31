70c6f9ae20697de9c258fe7a6e05f8fe05d33e91
Kurt Biery <biery@fnal.gov>
2023-05-31 06:18:32 -0500

    preliminary implementation of a generate_dromap_file function

--- python/dfmodules/integtest_file_gen.py
+++ python/dfmodules/integtest_file_gen.py
@@ -1,3 +1,4 @@
+import os
 
 def generate_hwmap_file(n_links, n_apps = 1, det_id = 3):
     conf="# DRO_SourceID DetLink DetSlot DetCrate DetID DRO_Host DRO_Card DRO_SLR DRO_Link\n"
@@ -16,3 +17,14 @@ def generate_hwmap_file(n_links, n_apps = 1, det_id = 3):
             conf+=f"{sid} {sid % 2} {sid // 2} {crate} {det_id} localhost {card} {slr} {link_num}\n"
             sid += 1
     return conf
+
+def generate_dromap_file():
+    #json_file=tmp_path_factory.getbasetemp() / "temp_dromap.json"
+    #json_file="/tmp/temp_dromap.json"
+    json_file="temp_dromap.json"
+    os.system(f"dromap_editor add-eth --src-id 0 --geo-stream-id 0 --geo-det-id 3 add-eth --src-id 1 --geo-stream-id 1 --geo-det-id 3 save {json_file} >> /dev/null")
+    #os.system("pwd")
+    map_text=""
+    for line in open(json_file).readlines():
+        map_text+=line
+    return map_text
