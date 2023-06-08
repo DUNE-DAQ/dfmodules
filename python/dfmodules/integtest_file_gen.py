a59ff0eff470136316c9b9858707d562f75d4169
Kurt Biery <biery@fnal.gov>
2023-06-08 12:05:51 -0500

    added a few necessary params for add_srcid with app_type='flx'

--- python/dfmodules/integtest_file_gen.py
+++ python/dfmodules/integtest_file_gen.py
@@ -19,7 +19,8 @@ def generate_hwmap_file(n_links, n_apps = 1, det_id = 3):
             sid += 1
     return conf
 
-def generate_dromap_contents(n_streams, n_apps = 1, det_id = 3, app_type = "eth", eth_protocol = "udp"):
+def generate_dromap_contents(n_streams, n_apps = 1, det_id = 3, app_type = "eth", app_host = "localhost",
+                             eth_protocol = "udp", flx_mode = "fix_rate", flx_protocol = "full"):
     the_map = dromap.DetReadoutMapService()
     source_id = 0
     for app in range(n_apps):
@@ -27,10 +28,11 @@ def generate_dromap_contents(n_streams, n_apps = 1, det_id = 3, app_type = "eth"
             geo_id = dromap.GeoID(det_id, app, 0, stream)
             if app_type == 'flx':
                 # untested!
-                the_map.add_srcid(source_id, geo_id, app_type,
+                the_map.add_srcid(source_id, geo_id, app_type, host=app_host,
+                                  protocol=flx_protocol, mode=flx_mode,
                                   card=app, slr=(stream // 5), link=(stream % 5))
             else:
-                the_map.add_srcid(source_id, geo_id, app_type, protocol=eth_protocol,
+                the_map.add_srcid(source_id, geo_id, app_type, protocol=eth_protocol, rx_host=app_host,
                                   rx_iface=app, rx_mac=f"00:00:00:00:00:0{app}", rx_ip=f"0.0.0.{app}")
             source_id += 1
     return json.dumps(the_map.as_json(), indent=4)
