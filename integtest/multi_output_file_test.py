2e73f06d7948ea5f56ed54fe2654744c210996cd
Kurt Biery <biery@fnal.gov>
2023-08-03 12:43:48 -0500

    Modified integtests to reflect the new location of the data_file_checks and dro_map_gen

--- integtest/multi_output_file_test.py
+++ integtest/multi_output_file_test.py
@@ -4,10 +4,10 @@ import re
 import copy
 import urllib.request
 
-import dfmodules.data_file_checks as data_file_checks
+import integrationtest.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
 import integrationtest.config_file_gen as config_file_gen
-import dfmodules.integtest_file_gen as integtest_file_gen
+import integrationtest.dro_map_gen as dro_map_gen
 
 # Values that help determine the running conditions
 number_of_data_producers=2
@@ -77,7 +77,7 @@ ignored_logfile_problems={"trigger": ["zipped_tpset_q: Unable to push within tim
 confgen_name="daqconf_multiru_gen"
 # The arguments to pass to the config generator, excluding the json
 # output directory (the test framework handles that)
-dro_map_contents = integtest_file_gen.generate_dromap_contents(number_of_data_producers, number_of_readout_apps)
+dro_map_contents = dro_map_gen.generate_dromap_contents(number_of_data_producers, number_of_readout_apps)
 
 conf_dict = config_file_gen.get_default_config_dict()
 conf_dict["daq_common"]["data_rate_slowdown_factor"] = data_rate_slowdown_factor
