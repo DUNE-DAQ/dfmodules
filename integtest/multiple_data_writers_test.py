64da1344dd7388c99bded6ab024f3e6bbb6e3aa2
Kurt Biery <biery@daq.fnal.gov>
2025-10-02 06:51:48 -0500

    Added first-pass resource checking to multiple_data_writers_test.py

--- integtest/multiple_data_writers_test.py
+++ integtest/multiple_data_writers_test.py
@@ -7,6 +7,7 @@ import urllib.request
 import integrationtest.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
 import integrationtest.data_classes as data_classes
+import integrationtest.resource_validation as resource_validation
 
 pytest_plugins = "integrationtest.integrationtest_drunc"
 
@@ -64,6 +65,16 @@ ignored_logfile_problems = {
     ],
 }
 
+# Determine if the conditions are right for this testing
+resval = resource_validation.ResourceValidator()
+resval.require_cpu_count(8)  # one per data source, plus a couple spares
+resval.require_free_memory_gb(10)  # approximately what we observe
+resval.require_total_memory_gb(20)  # safety factor of 2
+actual_output_path = "/tmp"
+resval.require_free_disk_space_gb(actual_output_path, 1)  # what we observe
+resval_debug_string = resval.get_debug_string()
+print(f"{resval_debug_string}")
+
 # The next three variable declarations *must* be present as globals in the test
 # file. They're read by the "fixtures" in conftest.py to determine how
 # to run the config generation and nanorc
@@ -119,18 +130,27 @@ confgen_arguments = {
 }
 
 # The commands to run in nanorc, as a list
-nanorc_command_list = (
-    "boot conf wait 5".split()
-    + "start --run-number 101 wait 1 enable-triggers wait 30".split()
-    + "disable-triggers wait 2 drain-dataflow wait 2 stop-trigger-sources stop ".split()
-    + "start --run-number 102 wait 1 enable-triggers wait 30".split()
-    + "disable-triggers wait 2 drain-dataflow wait 2 stop-trigger-sources stop ".split()
-    + " scrap terminate".split()
-)
+if resval.this_computer_has_sufficient_resources:
+    nanorc_command_list = (
+        "boot conf wait 5".split()
+        + "start --run-number 101 wait 1 enable-triggers wait 30".split()
+        + "disable-triggers wait 2 drain-dataflow wait 2 stop-trigger-sources stop ".split()
+        + "start --run-number 102 wait 1 enable-triggers wait 30".split()
+        + "disable-triggers wait 2 drain-dataflow wait 2 stop-trigger-sources stop ".split()
+        + " scrap terminate".split()
+    )
+else:
+    nanorc_command_list = ["wait", "1"]
 
 # The tests themselves
 
 def test_nanorc_success(run_nanorc):
+    if not resval.this_computer_has_sufficient_resources:
+        resval_report_string = resval.get_insufficient_resources_report()
+        print(f"{resval_report_string}")
+        resval_summary_string = resval.get_insufficient_resources_summary()
+        pytest.skip(f"{resval_summary_string}")
+
     current_test = os.environ.get("PYTEST_CURRENT_TEST")
     match_obj = re.search(r".*\[(.+)-run_nanorc0\].*", current_test)
     if match_obj:
@@ -144,6 +164,10 @@ def test_nanorc_success(run_nanorc):
 
 
 def test_log_files(run_nanorc):
+    if not resval.this_computer_has_sufficient_resources:
+        resval_summary_string = resval.get_insufficient_resources_summary()
+        pytest.skip(f"{resval_summary_string}")
+
     if check_for_logfile_errors:
         # Check that there are no warnings or errors in the log files
         assert log_file_checks.logs_are_error_free(
@@ -152,6 +176,10 @@ def test_log_files(run_nanorc):
 
 
 def test_data_files(run_nanorc):
+    if not resval.this_computer_has_sufficient_resources:
+        resval_summary_string = resval.get_insufficient_resources_summary()
+        pytest.skip(f"{resval_summary_string}")
+
     fragment_check_list = [triggercandidate_frag_params, hsi_frag_params, wibeth_frag_params]
     fragment_check_list.append(triggerprimitive_frag_params)
     fragment_check_list.append(triggeractivity_frag_params)
